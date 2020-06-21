0e8830c2077ae17ad07028c0dce015175cd08a11
Max <63409017+Bananamafia@users.noreply.github.com>
2020-06-21 21:06:58 +0200

    Adding a randomized Living Condition for Cell

--- ConwaysGameOfLife/Classes/Cell.cs
+++ ConwaysGameOfLife/Classes/Cell.cs
@@ -4,15 +4,28 @@ using System.Text;
 
 namespace ConwaysGameOfLife.Classes
 {
+
+
     class Cell
     {
-        public bool IsAlive { get; set; }
+        static bool _startLivingCondition()
+        {
+            Random rnd = new Random();
+            const int _maxValue = 100;
+            const int _livingChance = 30;
 
-        public int[] PositionOfCell { get; set; }
+            return rnd.Next(_maxValue) <= _livingChance;
+        }
+
+        public bool IsAlive { get; set; } = _startLivingCondition();
+
+        public int[,] PositionOfCell;
 
         List<Cell> NeighbourCells = new List<Cell>();
 
         public int LivingNeighbourCells { get; set; }
 
     }
+
+
 }
