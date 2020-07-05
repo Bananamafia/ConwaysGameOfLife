fc1efa928eaf659eda0970226ad315c412c65b52
Max <63409017+Bananamafia@users.noreply.github.com>
2020-07-05 20:44:29 +0200

    Created Converter between Cell.IsAlive and its color.

--- ConwaysGameOfLife/ViewModels/Converters/CellLivingStatusToCellColorConverter.cs
+++ ConwaysGameOfLife/ViewModels/Converters/CellLivingStatusToCellColorConverter.cs
@@ -1,16 +1,48 @@
-﻿using System;
+﻿using ConwaysGameOfLife.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace ConwaysGameOfLife.ViewModels.Converters
 {
-    class CellLivingStatusToCellColorConverter : INotifyPropertyChanged
+    public class CellLivingStatusToCellColorConverter : INotifyPropertyChanged, IValueConverter
     {
-        
-        
-        
-        
+        //---IValueConverter---
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            SolidColorBrush solidColorBrush = new SolidColorBrush();
+
+            if (value != null)
+            {
+                if (value.ToString() == "true")
+                {
+                    solidColorBrush.Color = System.Windows.Media.Color.FromRgb(0, 0, 0);
+                }
+                else
+                {
+                    solidColorBrush.Color = System.Windows.Media.Color.FromRgb(255, 255, 255);
+                }
+
+                return solidColorBrush;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //---INotifyPropertyChanged---
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string property)
         {
