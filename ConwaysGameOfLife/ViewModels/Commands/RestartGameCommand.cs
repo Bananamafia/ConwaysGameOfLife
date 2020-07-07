353e2061b87af5dac61d7163277e64127d9ec161
Max <63409017+Bananamafia@users.noreply.github.com>
2020-07-07 18:10:02 +0200

    Added RestartGameCommand

---
+++ ConwaysGameOfLife/ViewModels/Commands/RestartGameCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ConwaysGameOfLife.ViewModels.Commands
+{
+    class RestartGameCommand : ICommand
+    {
+        private Action _execute;
+        
+        public RestartGameCommand(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke();
+        }
+    }
+}
