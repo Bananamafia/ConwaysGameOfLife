e79eb65440469f1bfa872eb1150dd8650b1b41b6
Max <63409017+Bananamafia@users.noreply.github.com>
2021-06-12 23:06:34 +0200

    Added first implementation of Cell class
    Added Properties for Living Status and Position on Grid

---
+++ Conways.DesktopApp/Models/ConwaysCell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conways.DesktopApp.Models
+{
+    public class ConwaysCell
+    {
+        public ConwaysCell(int xCoordinate, int yCoordinate)
+        {
+            Random random = new();
+
+            PositionOnXAxis = xCoordinate;
+            PositionOnYAxis = yCoordinate;
+
+            IsAlive = random.Next(0, 100) <= initialLivingChanceInPercent;
+        }
+
+        private double initialLivingChanceInPercent = 30;
+        public bool IsAlive { get; set; }
+        public bool IsAliveInNextTurn { get; set; }
+
+        public int PositionOnXAxis { get; init; }
+        public int PositionOnYAxis { get; init; }
+
+    }
+}
