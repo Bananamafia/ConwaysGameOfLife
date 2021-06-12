08d26d4aa3749009e1f7e66b4ea54ef26657c95d
Max <63409017+Bananamafia@users.noreply.github.com>
2021-06-12 22:52:51 +0200

    Add Gameboard initialization
    Added function which draws a grid with variable amount of cloumns and rows
    Added function which adds boarder around each grid cell

--- Conways.DesktopApp/Views/MainView.xaml.cs
+++ Conways.DesktopApp/Views/MainView.xaml.cs
@@ -22,6 +22,49 @@ namespace Conways.DesktopApp.Views
         public MainView()
         {
             InitializeComponent();
+            DrawGameBoard(384, 203);
+
+            DrawBorderAroundGridCells();
+        }
+
+        public void DrawGameBoard(int columns, int rows)
+        {
+            Grid gameBoard = new Grid();
+
+            for (int i = 0; i < columns; i++)
+            {
+                //ColumnDefinition columnDefinition = new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) };
+                ColumnDefinition columnDefinition = new ColumnDefinition() { Width = new GridLength(5) };
+                gameBoard.ColumnDefinitions.Add(columnDefinition);
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                //RowDefinition rowDefinition = new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) };
+                RowDefinition rowDefinition = new RowDefinition() { Height = new GridLength(5) };
+                gameBoard.RowDefinitions.Add(rowDefinition);
+            }
+
+            RegisterName("GameBoardGrid", gameBoard);
+            MainGrid.Children.Add(gameBoard);
+        }
+
+        public void DrawBorderAroundGridCells()
+        {
+            Grid gameBoardGrid = FindName("GameBoardGrid") as Grid;
+            int columns = gameBoardGrid.ColumnDefinitions.Count();
+            int rows = gameBoardGrid.RowDefinitions.Count();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Border border = new Border() { BorderBrush = Brushes.LightGray, BorderThickness = new Thickness(1) };
+                    Grid.SetColumn(border, j);
+                    Grid.SetRow(border, i);
+                    gameBoardGrid.Children.Add(border);
+                }
+            }
         }
     }
 }
