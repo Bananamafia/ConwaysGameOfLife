0b5def8d19e76f1cd9a54ac1e5e6973cb58435a1
Bananamafia <maximilian.kiefer@yahoo.de>
2021-06-13 17:49:54 +0200

    Changed the AddNeighboursOfConwayCells to Task, to improve speed

--- Conways.DesktopApp/ViewModels/MainViewModel.cs
+++ Conways.DesktopApp/ViewModels/MainViewModel.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Conways.DesktopApp.ViewModels
 {
@@ -11,8 +13,8 @@ namespace Conways.DesktopApp.ViewModels
     {
         public MainViewModel()
         {
-            InstantiateConwayCells(10, 10);
-            AddNeighboursOfConwayCells();
+            InstantiateConwayCells(200, 100);
+            AddNeighboursOfConwayCellsAsync();
         }
 
         public List<ConwayCell> MyConwayCells { get; set; }
@@ -30,72 +32,79 @@ namespace Conways.DesktopApp.ViewModels
             }
         }
 
-        private void AddNeighboursOfConwayCells()
+        private async Task AddNeighboursOfConwayCellsAsync()
         {
             int maxXCoordinate = MyConwayCells.Last().PositionOnXAxis;
             int maxYCoordninate = MyConwayCells.Last().PositionOnYAxis;
 
+            List<Task> tasks = new();
+
             foreach (var conwayCell in MyConwayCells)
             {
-                int topRow, sameRow, bottomRow;
-                int leftColumn, sameColumn, rightColumn;
+                tasks.Add(Task.Run(() =>
+                {
+                    int topRow, sameRow, bottomRow;
+                    int leftColumn, sameColumn, rightColumn;
 
-                if (conwayCell.PositionOnYAxis != 0)
-                { topRow = conwayCell.PositionOnYAxis - 1; }
-                else
-                { topRow = maxYCoordninate; }
+                    if (conwayCell.PositionOnYAxis != 0)
+                    { topRow = conwayCell.PositionOnYAxis - 1; }
+                    else
+                    { topRow = maxYCoordninate; }
 
-                sameRow = conwayCell.PositionOnYAxis;
+                    sameRow = conwayCell.PositionOnYAxis;
 
-                if (conwayCell.PositionOnYAxis != maxYCoordninate)
-                { bottomRow = conwayCell.PositionOnYAxis + 1; }
-                else
-                { bottomRow = 0; }
+                    if (conwayCell.PositionOnYAxis != maxYCoordninate)
+                    { bottomRow = conwayCell.PositionOnYAxis + 1; }
+                    else
+                    { bottomRow = 0; }
 
 
-                if (conwayCell.PositionOnXAxis != 0)
-                { leftColumn = conwayCell.PositionOnXAxis - 1; }
-                else
-                { leftColumn = maxXCoordinate; }
+                    if (conwayCell.PositionOnXAxis != 0)
+                    { leftColumn = conwayCell.PositionOnXAxis - 1; }
+                    else
+                    { leftColumn = maxXCoordinate; }
 
-                sameColumn = conwayCell.PositionOnXAxis;
+                    sameColumn = conwayCell.PositionOnXAxis;
 
-                if (conwayCell.PositionOnXAxis != maxXCoordinate)
-                { rightColumn = conwayCell.PositionOnXAxis + 1; }
-                else
-                { rightColumn = 0; }
+                    if (conwayCell.PositionOnXAxis != maxXCoordinate)
+                    { rightColumn = conwayCell.PositionOnXAxis + 1; }
+                    else
+                    { rightColumn = 0; }
 
-                conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
-                    cell.PositionOnXAxis == leftColumn &&
-                    cell.PositionOnYAxis == topRow));
+                    conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
+                        cell.PositionOnXAxis == leftColumn &&
+                        cell.PositionOnYAxis == topRow));
 
-                conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
-                      cell.PositionOnXAxis == sameColumn &&
-                      cell.PositionOnYAxis == topRow));
+                    conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
+                          cell.PositionOnXAxis == sameColumn &&
+                          cell.PositionOnYAxis == topRow));
 
-                conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
-                    cell.PositionOnXAxis == rightColumn &&
-                    cell.PositionOnYAxis == topRow));
+                    conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
+                        cell.PositionOnXAxis == rightColumn &&
+                        cell.PositionOnYAxis == topRow));
 
-                conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
-                    cell.PositionOnXAxis == leftColumn &&
-                    cell.PositionOnYAxis == sameRow));
+                    conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
+                        cell.PositionOnXAxis == leftColumn &&
+                        cell.PositionOnYAxis == sameRow));
 
-                conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
-                    cell.PositionOnXAxis == rightColumn &&
-                    cell.PositionOnYAxis == sameRow));
+                    conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
+                        cell.PositionOnXAxis == rightColumn &&
+                        cell.PositionOnYAxis == sameRow));
 
-                conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
-                    cell.PositionOnXAxis == leftColumn &&
-                    cell.PositionOnYAxis == bottomRow));
+                    conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
+                        cell.PositionOnXAxis == leftColumn &&
+                        cell.PositionOnYAxis == bottomRow));
 
-                conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
-                    cell.PositionOnXAxis == sameColumn &&
-                    cell.PositionOnYAxis == bottomRow));
+                    conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
+                        cell.PositionOnXAxis == sameColumn &&
+                        cell.PositionOnYAxis == bottomRow));
 
-                conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
-                    cell.PositionOnXAxis == rightColumn &&
-                    cell.PositionOnYAxis == bottomRow));
+                    conwayCell.NeighbourCells.Add(MyConwayCells.Find(cell =>
+                        cell.PositionOnXAxis == rightColumn &&
+                        cell.PositionOnYAxis == bottomRow));
+                }));
+
+                await Task.WhenAll(tasks);
             }
         }
     }
